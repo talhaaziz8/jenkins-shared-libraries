7808a58a551730bb2a914f6860807f52ac366f82
Talha Aziz <aziz81812@gmail.com>
2025-02-26 19:20:21 +0500

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,4 +1,4 @@
-def call(String credId, String imageName) {
+def call(String credId, String imageName, String tag = "latest") {
     withCredentials([usernamePassword(
         credentialsId: credId,
         passwordVariable: "dockerHubPass",
@@ -8,13 +8,11 @@ def call(String credId, String imageName) {
         sh "echo ${env.dockerHubPass} | docker login -u ${env.dockerHubUser} --password-stdin"
 
         echo "🏗️ Tagging Docker image..."
-        sh "docker tag ${imageName} ${env.dockerHubUser}/${imageName}:latest"
+        sh "docker tag ${imageName} ${env.dockerHubUser}/${imageName}:${tag}"
 
         echo "📤 Pushing Docker image..."
-        sh "docker push ${env.dockerHubUser}/${imageName}:latest"
+        sh "docker push ${env.dockerHubUser}/${imageName}:${tag}"
         
-        echo "✅ Successfully pushed ${env.dockerHubUser}/${imageName}:latest to Docker Hub!"
+        echo "✅ Successfully pushed ${env.dockerHubUser}/${imageName}:${tag} to Docker Hub!"
     }
 }
-
-
